4c6b6b02fbdf1fd0f41688c48868e75ade413e52
Bogdan Costea <bogdan@costea.us>
2015-05-27 01:04:02 +0300

    added about page to security whitelist

--- src/main/java/com/itakeunconf/codecraft/configuration/SecurityConfiguration.java
+++ src/main/java/com/itakeunconf/codecraft/configuration/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/login").permitAll()              // everybody can see the login page
-                    .antMatchers("/","/index*").permitAll()         // everybody can see the landing
+                    .antMatchers("/","/index*","/about*").permitAll() // everybody can see the landing
                     .antMatchers("/api/public/*").permitAll()       // allow access to public API
                     .antMatchers("/admin").hasRole("ADMIN")         // only admins can see the administration area
                     .anyRequest().authenticated()
