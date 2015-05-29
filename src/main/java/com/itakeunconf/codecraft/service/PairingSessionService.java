744ae2a1d3384994b809391e36eeab7980ec70a0
Bogdan Costea <bogdan@costea.us>
2015-05-29 10:33:47 +0300

    refactored pairing session classes

---
+++ src/main/java/com/itakeunconf/codecraft/service/PairingSessionService.java
@@ -0,0 +1,7 @@
+package com.itakeunconf.codecraft.service;
+
+/**
+ * Created by BogdanCo on 2015-05-29.
+ */
+public interface PairingSessionService {
+}
---
+++ src/test/java/com/itakeunconf/codecraft/service/impl/DefaultPairingSessionServiceTest.java
@@ -0,0 +1,7 @@
+package com.itakeunconf.codecraft.service.impl;
+
+/**
+ * Created by BogdanCo on 2015-05-29.
+ */
+public class DefaultPairingSessionServiceTest {
+}
